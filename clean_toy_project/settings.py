c8eae5582706a3dc902d908abfef707bec1555f1
taxijjang <gw9122@naver.com>
2021-06-18 00:02:46 +0900

    modified settings.py

--- clean_toy_project/settings.py
+++ clean_toy_project/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+from .secret import SECRET
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -70,12 +70,7 @@ WSGI_APPLICATION = 'clean_toy_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = SECRET['DATABASE_DEV']
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
