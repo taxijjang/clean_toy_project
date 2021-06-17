f8c2adb5dcaa9927dda5e41f4a1911b019f760e7
taxijjang <gw9122@naver.com>
2021-06-18 00:03:16 +0900

    Added crwaling news to db

--- news/tasks.py
+++ news/tasks.py
@@ -3,36 +3,40 @@ import requests
 from celery import shared_task
 from bs4 import BeautifulSoup
 
+from django.db import transaction
 from news.models.flatform import FlatForm
 from news.models.category import Category
 from news.models.story import Story
 
 
 @shared_task
-def add(x, y):
-    return x + y
-
-
-
 def crawlings():
+    news_list = []
     for flatform in FlatForm.objects.all():
-        if flatform.title == 'naver':
-            continue
-        news_list = []
         for category in flatform.categories.all():
             request = requests.get(category.full_url)
             soup = BeautifulSoup(request.text, 'html.parser')
 
             news = soup.select(flatform.select_location)
+
+            if not news:
+                break
+
             titles = news[0].find_all('a', class_=flatform.a_tag_class_name)
 
             for rank, title in enumerate(titles):
-                news_data = dict(
-                    rank=rank+1,
+                story = Story(
+                    flatform=flatform,
+                    category=category,
+                    rank=rank + 1,
                     title=title.text,
-                    url=flatform.url + title.get('href') if flatform.title == 'naver' else title.get('href')
+                    url=flatform.url[:-1] + title.get('href') if flatform.title == 'naver' else title.get('href')
                 )
-                news_list.append(news_data)
+                news_list.append(story)
+    update_news(news_list)
 
-                print(flatform.title, category.title, news_data)
-        print(news_list)
+
+@transaction.atomic
+def update_news(news_list):
+    Story.truncate()
+    Story.objects.bulk_create(news_list)
