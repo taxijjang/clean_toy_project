9ff8a18274eed12025ea3a85f8b096c589f6c251
taxijjang <gw9122@naver.com>
2021-07-04 14:06:31 +0900

    init

--- news/serializers/category_sz.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from news.models.category import Category
-
-
-class CategoryListSZ(ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ('id', 'title')
--- news/serializers/story_sz.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from news.models.story import Story
-
-
-class StoryListSZ(ModelSerializer):
-    class Meta:
-        model = Story
-        fields = ('id', 'title', 'rank', 'url')
