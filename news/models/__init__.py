2e65e8ad44dde024dede8f9c8468bf714c7f8207
taxijjang <gw9122@naver.com>
2021-07-04 14:07:59 +0900

    init

--- news/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from news.models.flatform import *
-from news.models.category import *
-from news.models.story import *
--- news/serializers/flatform_sz.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from news.models.flatform import FlatForm
-
-
-class FlatformListSZ(ModelSerializer):
-    class Meta:
-        model = FlatForm
-        fields = ('id', 'title')
