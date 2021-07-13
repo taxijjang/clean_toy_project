716d0d63cf7f30218ce5192bea5e8cb40b5eabd3
taxijjang <gw9122@naver.com>
2021-07-13 21:28:49 +0900

    Add get_queryset in list

--- news/views/story_views.py
+++ news/views/story_views.py
@@ -11,16 +11,14 @@ class StoryListView(ListAPIView):
     queryset = Story.objects.all()
     serializer_class = StoryListSZ
 
-    def list(self, request, *args, **kwargs):
-        pass
-    # def get_queryset(self):
-    #     flat_form_title = self.kwargs.get('flatform')
-    #     category_title = self.kwargs.get('category')
-    #
-    #     if not FlatForm.objects.filter(title=flat_form_title):
-    #         raise exceptions.NotFound(detail='flatform not exists')
-    #
-    #     if not Category.objects.filter(title=category_title):
-    #         raise exceptions.NotFound(detail='category not exists')
-    #
-    #     return self.queryset.filter(flat_form__title=flat_form_title, category__title=category_title)
+    def get_queryset(self):
+        flat_form_title = self.kwargs.get('flatform')
+        category_title = self.kwargs.get('category')
+
+        if not FlatForm.objects.filter(title=flat_form_title):
+            raise exceptions.NotFound(detail='flatform not exists')
+
+        if not Category.objects.filter(title=category_title):
+            raise exceptions.NotFound(detail='category not exists')
+
+        return self.queryset.filter(flat_form__title=flat_form_title, category__title=category_title)
