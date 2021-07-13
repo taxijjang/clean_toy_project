9752e1878cbb34f291df6786b290c6d8fb713e3f
taxijjang <gw9122@naver.com>
2021-07-13 20:11:10 +0900

    DataClass

--- news/tasks.py
+++ news/tasks.py
@@ -8,7 +8,7 @@ from domain.repositories import StoryReader
 from domain.repositories import StoryWriter
 from news.models.flatform import FlatForm
 from news.models.story import Story
-
+from domain.entity import Story as DataClass
 
 @shared_task
 def crawlings():
@@ -17,26 +17,22 @@ def crawlings():
         for category in flatform.categories.all():
             request = requests.get(category.full_url)
             soup = BeautifulSoup(request.text, 'html.parser')
-
             news = soup.select(flatform.select_location)
-
             if not news:
                 break
 
             titles = news[0].find_all('a', class_=flatform.a_tag_class_name)
-
             for rank, title in enumerate(titles):
-                data = dict(
+                data = DataClass(
                     flat_form=flatform,
                     category=category,
                     rank=rank + 1,
                     title=title.text,
                     url=flatform.url[:-1] + title.get('href') if flatform.title == 'naver' else title.get('href')
-                ) # data class 로 저장
+                )
                 story_service = StoryService(
                     reader=StoryReader(),
                     writer=StoryWriter(),
                     data=data
                 )
-
                 story_service.execute()
